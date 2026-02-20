ed5f1ff8a9cb642370261be836c95ed731cd236b
AlaBouhaouel <ellabouhawel@gmail.com>
2026-02-20 23:25:46 +0300

    deploying

--- covise/covise/settings.py
+++ covise/covise/settings.py
@@ -84,7 +84,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # HTTPS Security (only in production)
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = False  # Railway handles this
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
