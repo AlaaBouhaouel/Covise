337b34d817f349117af0e7c64bd89e42ea02ab42
AlaBouhaouel <ellabouhawel@gmail.com>
2026-02-21 21:22:28 +0300

    fix CSRF for admin

--- covise/covise/settings.py
+++ covise/covise/settings.py
@@ -87,8 +87,8 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = False  # temporarily disable
+    CSRF_COOKIE_SECURE = False     # temporarily disable
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 
